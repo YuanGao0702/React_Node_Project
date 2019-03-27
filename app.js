ef90d682e73903fb3be97436a73a22cbf7d8c8f0
Gary Gao <yuan.g.gao@foxconn.com>
2019-03-27 17:55:45 -0400

    upload to github

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'Client/build')));
+
+// Put all API endpoints under '/api'
+app.get('/allusers', function(req, res, next) {
+  res.json([{
+  	id: 1,
+  	username: "gary"
+  }, {
+  	id: 2,
+  	username: "summer"
+  }]);
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/Client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`listening on ${port}`);
