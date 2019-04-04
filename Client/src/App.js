1adabc9bacae1d03afd2fa9e241b6299eedf4441
Gary Gao <yuan.g.gao@foxconn.com>
2019-04-03 20:33:27 -0400

    add button clicked

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,23 +5,29 @@ import './App.css';
 class App extends Component {
 
   state = {users: []}
-  weather = {}
+  weatherJson = {}
   componentDidMount() {
     //this.download()
     //this.getAllUsers()
     this.getWeather()
+    //this.download()
+    // this.buttonClicked()
+    this.download()
   }
+
   getWeather=()=>{
     fetch('/weather')
       .then(res => res.json())
-      .then(weather => {
-        this.weather = weather
+      .then(data => {
+        this.weatherJson = data
+        document.getElementById('text1').innerHTML = this.weatherJson.Weather;
       });
   }
   getAllUsers=()=>{
     fetch('/allusers')
       .then(res => res.json())
-      .then(users => this.setState({ users }));
+      .then(users => this.setState({ users })
+    )
   }
   download=()=>{
     fetch('/pic')
@@ -32,28 +38,34 @@ class App extends Component {
           let filename = 'funny.gif';
           a.href = blobUrl;
           a.download = filename;
-          //a.click();
-          //window.URL.revokeObjectURL(blobUrl);
         })
       })
   }
-
   _handleKeyPress = (e) => {
     if (e.key === 'Enter') {
-      //alert("Enter Pressed!")
       var input1 = document.getElementById('input1').value;
       if(input1=="summer"){
-          document.getElementById('text1').innerHTML = this.weather.Weather;
+        this.buttonClicked()
       }
     }
   }
+  buttonClicked=()=> {
+      var x = document.getElementById('text1')
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+  }
+
   render() {
 
     return (
       <div className="App">
-        <a id="a_id" onclick={this.download()}><img src="/pic"></img></a>
+        <a id="a_id" onclick={this.download}><img src="/pic"></img></a>
         <p><input type = "text" id = "input1" onKeyPress={this._handleKeyPress}></input></p>
-        <p id ="text1"></p>
+        <p id ="text1" style={{display:"none"}}></p>
+        <button onClick={this.buttonClicked}>Weather</button>
         {this.state.users.map(user =>
           <div key={user.id}>{user.username}</div>
         )}
